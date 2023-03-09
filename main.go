806cd55cf128dfcdaa497575785cda340af15769
tkanata <tkanata@netprotections.co.jp>
2023-03-09 13:25:54 +0900

    feat: 引数として渡した文字列を表示する機能を追加

--- main.go
+++ main.go
@@ -4,15 +4,26 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/urfave/cli/v2"
+	cli "github.com/urfave/cli/v2"
 )
 
 func main() {
 	app := &cli.App{
-		Name:  "やるぞやるぞやるぞ",
-		Usage: "say やるぞやるぞやるぞ",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "yaruzoyaruzoyaruzo",
+				Value: "empty",
+				Usage: "say やるぞやるぞやるぞ",
+			},
+		},
 		Action: func(c *cli.Context) error {
-			fmt.Println("やるぞやるぞやるぞ")
+			// fmt.Println("やるぞやるぞやるぞ")
+			yaruzo := c.String("yaruzoyaruzoyaruzo")
+			if yaruzo == "やるぞやるぞやるぞ" {
+				fmt.Println("やるぞやるぞやるぞ")
+			} else {
+				fmt.Printf("%v\n", yaruzo)
+			}
 			return nil
 		},
 	}
